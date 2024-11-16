8717bc4fe588ece247b91f5ea4b82818c8fad3ea
Hamisi_Juma <hamisig123@gmail.com>
2024-11-16 20:14:11 +0300

    initial commit

---
+++ app/src/main/java/com/example/newtamutamu/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.newtamutamu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int SPLASH_TIMEOUT = 3000; // 3 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splashscren);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
+    }
+}
